c53bfa7315fbe0e2d258d4df4b6a436dc8edb00e
Subrata Rajak <subrata324cse@gmail.com>
2022-04-11 19:20:20 +0530

    Dice Game Completed

---
+++ index.js
@@ -0,0 +1,31 @@
+var counter = 0;
+
+document.querySelector(".refresh_btn").addEventListener("click", function () {
+    var randomNumberOne = Math.floor(Math.random() * 6) + 1;
+
+    var randomDiceImageOne = "dice" + randomNumberOne + ".png";
+
+    var randomDiceImageSourceOne = "Images/" + randomDiceImageOne;
+
+    document.querySelector(".img1").setAttribute("src", randomDiceImageSourceOne);
+
+    var randomNumberTwo = Math.floor(Math.random() * 6) + 1;
+
+    var randomDiceImageTwo = "dice" + randomNumberTwo + ".png";
+
+    var randomDiceImageSourceTwo = "Images/" + randomDiceImageTwo;
+
+    document.querySelector(".img2").setAttribute("src", randomDiceImageSourceTwo);
+
+    if (randomNumberOne > randomNumberTwo) {
+        document.querySelector("h1").innerHTML = "Player 1 Wins";
+    } else if (randomNumberTwo > randomNumberOne) {
+        document.querySelector("h1").innerHTML = "Payer 2 Wins";
+    } else {
+        document.querySelector("h1").innerHTML = "It's a Draw!";
+    } 
+
+    counter++;
+    document.querySelector(".play_time").innerHTML = "You played " + counter + " times";
+});
+
